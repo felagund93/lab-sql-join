7a3daa0b4453b4b0b9c808de511108ad305dcbca
Albert Godoy Hernandez <alberto.godoy.h@gmail.com>
2021-11-09 21:23:20 +0100

    commiting empty lab to online repo

---
+++ lab_sql_join.sql
@@ -0,0 +1,10 @@
+# Lab | SQL Join
+# In this lab, you will be using the Sakila database of movie rentals.
+
+# Instructions
+## List number of films per category.
+## Display the first and last names, as well as the address, of each staff member.
+## Display the total amount rung up by each staff member in August of 2005.
+## List each film and the number of actors who are listed for that film.
+## Using the tables payment and customer and the JOIN command, list the total paid by each customer. 
+# List the customers alphabetically by last name.
